6e2cfc87ac4cbe7ae6e32dc325f57372d8b824a8
Abdulrahman Alshammari <mrabdulrahman95@gmail.com>
2025-03-19 21:15:36 +0300

    fixed up the moving logic

--- Assets/scripts/Mover.cs
+++ Assets/scripts/Mover.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
-    float xValue = 0.0f;
-    float yValue = 0.01f;
-    float zValue = 0.0f;
+
+    [SerializeField] float moveSpeed =15f;
+
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(xValue,yValue,zValue);
+    void FixedUpdate() {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0.0f;
+        float zValue = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(xValue, yValue, zValue)*moveSpeed;
+        rb.linearVelocity=movement;
     }
 }
